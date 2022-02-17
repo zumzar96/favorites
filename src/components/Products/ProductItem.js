2bbd57ced1043ee588ee1a2479aa95293620cda4
ismail <ismailgim96@gmail.com>
2022-02-17 13:12:21 +0100

    Finishing custom hook

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,15 +1,16 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import Card from '../UI/Card';
-import { ProductsContext } from '../../context/products-context';
+import { useStore } from '../../hooks-store/store';
 import './ProductItem.css';
 
-const ProductItem = props => {
-
-    const toggleFav = useContext(ProductsContext).toggleFav;
+const ProductItem = React.memo(props => {
+    console.log('RENDERING');
+    const dispatch = useStore(false)[1];
 
     const toggleFavHandler = () => {
-        toggleFav(props.id);
+        // toggleFav(props.id);
+        dispatch('TOGGLE_FAV', props.id);
     };
 
     return (
@@ -26,6 +27,6 @@ const ProductItem = props => {
             </div>
         </Card>
     );
-};
+});
 
 export default ProductItem;
